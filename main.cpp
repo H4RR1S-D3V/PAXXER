fcfd76fa244aae814f53b9a0ae2807b60fe68e6a
Luxary <f.leiton001@gmail.com>
2025-01-02 17:17:25 -0300

    fix-dibujos-menu-principal

--- main.cpp
+++ main.cpp
@@ -5,13 +5,12 @@
 #include "./view/menuPrincipal.h"
 #include "./view/pantallasMenuPrincipal.h"
 
+
 int main()
 {
+
     HWND consola = GetConsoleWindow();
     ShowWindow(consola, SW_MAXIMIZE);
-
-
-
     mostrarMenuPrincipal();
     return 0;
 }
