6df0430d8d5fc480307dd5bec189f03237e95f4c
Luxary <f.leiton001@gmail.com>
2025-01-06 14:00:48 -0300

    fix-fullscreen

--- main.cpp
+++ main.cpp
@@ -6,11 +6,18 @@
 #include "./view/pantallasMenuPrincipal.h"
 
 
-int main()
-{
 
+int main() {
+    /// PONER CONSOLA EN FULLSCREEN
     HWND consola = GetConsoleWindow();
-    ShowWindow(consola, SW_MAXIMIZE);
+    if (consola) {
+        // Enviar combinación de teclas ALT + ENTER para pantalla completa
+        keybd_event(VK_MENU, 0x38, 0, 0);       // Presionar ALT
+        keybd_event(VK_RETURN, 0x1C, 0, 0);     // Presionar ENTER
+        keybd_event(VK_RETURN, 0x1C, KEYEVENTF_KEYUP, 0); // Soltar ENTER
+        keybd_event(VK_MENU, 0x38, KEYEVENTF_KEYUP, 0);   // Soltar ALT
+    }
+    /// EJECUCION DE LA APP
     mostrarMenuPrincipal();
     return 0;
 }
